d95caa47e47b669bc32f8a91cad98bc2aebf880c
soral215 <68497163+soral215@users.noreply.github.com>
2020-11-04 18:46:49 +0900

    Update chat.js

--- chat.js
+++ chat.js
@@ -12,11 +12,29 @@ const Chat = (function(){
             }
         });
     }
+    // 메세지 전송
+    function sendMessage(message) {
+        // 서버에 전송하는 코드로 후에 대체
+        const data = {
+            "senderName"    : "blue",
+            "message"        : message
+        };
+ 
+        // 통신하는 기능이 없으므로 여기서 receive
+        resive(data);
+    }
+ 
     // 메세지 입력박스 내용 지우기
     function clearTextarea() {
         $('div.input-div textarea').val('');
     }
  
+    // 메세지 수신
+    function resive(data) {
+        const LR = (data.senderName != myName)? "left" : "right";
+        appendMessageTag("right", data.senderName, data.message);
+    }
+
     return {
         'init': init
     };
